95f73184423f0c30896855f44d6134031e380a9b
SirIsaacNeutron <dbadir@cox.net>
2019-02-26 14:11:43 -0800

    Added money formatting

--- src/components/ValuesTable.js
+++ src/components/ValuesTable.js
@@ -15,8 +15,12 @@ class ValuesTable extends Component {
     for (let i = 0; i < 13; ++i) {
       tableElements.push(
         <tr key={i + 1}>
-          <ValuesTableCell case={this.props.cases[i]} />
-          <ValuesTableCell case={this.props.cases[i + 13]} />
+          <ValuesTableCell
+            case={this.props.cases[i]}
+            formatMoney={this.props.formatMoney}/>
+          <ValuesTableCell
+            case={this.props.cases[i + 13]}
+            formatMoney={this.props.formatMoney}/>
         </tr>
       );
     }
@@ -33,6 +37,7 @@ class ValuesTable extends Component {
 
 ValuesTable.propTypes = {
   cases: PropTypes.array.isRequired,
+  formatMoney: PropTypes.func.isRequired,
 };
 
 export default ValuesTable;
