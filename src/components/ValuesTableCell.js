05fef8ceb7d86e99aa05681c394d21ab7f06ae90
SirIsaacNeutron <dbadir@cox.net>
2019-02-26 14:19:44 -0800

    Fixed money formatting
    Apologies for the ugly git history. I'm still not the best at git.

--- src/components/ValuesTableCell.js
+++ src/components/ValuesTableCell.js
@@ -4,12 +4,15 @@ import PropTypes from 'prop-types';
 class ValuesTableCell extends Component {
   render() {
     const caseValue = this.props.case.value;
+    let caseValueString = caseValue === 0.01 ?
+      "$0.01" : this.props.formatMoney(caseValue, 0);
+
     return this.props.case.opened ?
       <td class="ValuesTableCell" style={{backgroundColor: "#888"}}>
-        {this.props.formatMoney(caseValue, 0)}
+        {caseValueString}
       </td> :
       <td class="ValuesTableCell" style={{backgroundColor: "#ff6"}}>
-        {this.props.formatMoney(caseValue, 0)}
+        {caseValueString}
       </td>
   }
 }
