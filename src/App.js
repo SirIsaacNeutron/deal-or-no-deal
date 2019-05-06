f5a5f1d472900c69e8936285622f1e8305c062b1
SirIsaacNeutron <dbadir@cox.net>
2019-05-06 16:05:38 -0700

    Fixed formatting

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ import TurnInfo from './components/TurnInfo';
 import './App.css';
 
 const CASE_VALUES = [0.01, 1, 5, 10, 25, 50, 75, 100,
-	200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000,
-	75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
+  200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000,
+  75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000];
 
 const ORIGINAL_NUM_CHOICES = 6;
 
 function shuffle(array) {
-	// Copied from Stack Overflow
-	// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
-	let counter = array.length;
+  // Copied from Stack Overflow
+  // https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+  let counter = array.length;
 
   // While there are elements in the array
-	while (counter > 0) {
+  while (counter > 0) {
       // Pick a random index
       let index = Math.floor(Math.random() * counter);
       // Decrease counter by 1
@@ -39,22 +39,22 @@ class App extends Component {
     super(props);
     this.state = {
       cases: this.createCases(),
-			initialCaseChosen: false,
-			initialCase: '',
-			initialCaseNum: 0,
-			numCasesToChoose: ORIGINAL_NUM_CHOICES,
-			caseChosenNormally: '',
-			offer: 0,
-			turnNum: 1,
-			numCasesRemaining: 26,
-			finalOfferMade: false,
+      initialCaseChosen: false,
+      initialCase: '',
+      initialCaseNum: 0,
+      numCasesToChoose: ORIGINAL_NUM_CHOICES,
+      caseChosenNormally: '',
+      offer: 0,
+      turnNum: 1,
+      numCasesRemaining: 26,
+      finalOfferMade: false,
     }
   }
 
   createCases = () => {
     let cases = [];
 
-		let randomCaseValues = shuffle([...CASE_VALUES]);
+    let randomCaseValues = shuffle([...CASE_VALUES]);
 
     randomCaseValues.forEach((value, index) => {
       cases.push({
@@ -67,132 +67,132 @@ class App extends Component {
     return cases;
   }
 
-	chooseCase = (caseNum) => {
-		if (!this.state.initialCaseChosen) {
-			this.setState({
-				initialCaseChosen: true,
-				initialCase: this.state.cases[caseNum - 1],
-				initialCaseNum: caseNum,
-			});
-		}
-		else {
-			this.state.cases[caseNum - 1].opened = true;
-			this.setState({
-				numCasesToChoose: this.state.numCasesToChoose - 1,
-				caseChosenNormally: this.state.cases[caseNum - 1],
-			});
-		}
+  chooseCase = (caseNum) => {
+    if (!this.state.initialCaseChosen) {
+      this.setState({
+        initialCaseChosen: true,
+        initialCase: this.state.cases[caseNum - 1],
+        initialCaseNum: caseNum,
+      });
+    }
+    else {
+      this.state.cases[caseNum - 1].opened = true;
+      this.setState({
+        numCasesToChoose: this.state.numCasesToChoose - 1,
+        caseChosenNormally: this.state.cases[caseNum - 1],
+      });
+    }
 
-		this.setState({
-			numCasesRemaining: this.state.numCasesRemaining - 1,
-		});
+    this.setState({
+      numCasesRemaining: this.state.numCasesRemaining - 1,
+    });
 
-		if (this.state.numCasesToChoose - 1 === 0) {
-			this.calculateOffer();
-		}
-	}
+    if (this.state.numCasesToChoose - 1 === 0) {
+      this.calculateOffer();
+    }
+  }
 
-	calculateOffer = () => {
-		// Using a rough formula from this link:
-		// https://answers.yahoo.com/question/index?qid=20061106173902AAc48qj
-		const nonOpenedCases = [...this.state.cases.filter(c => !c.opened)];
-		const valueOfCases = nonOpenedCases.reduce(
-			(accumulator, c) => {
-				return accumulator + c.value;
-		}, 0);
-		const averageValueOfCases = valueOfCases / nonOpenedCases.length;
-		const newOffer =
-			(averageValueOfCases * (this.state.turnNum / 10)).toFixed(2);
+  calculateOffer = () => {
+    // Using a rough formula from this link:
+    // https://answers.yahoo.com/question/index?qid=20061106173902AAc48qj
+    const nonOpenedCases = [...this.state.cases.filter(c => !c.opened)];
+    const valueOfCases = nonOpenedCases.reduce(
+      (accumulator, c) => {
+        return accumulator + c.value;
+    }, 0);
+    const averageValueOfCases = valueOfCases / nonOpenedCases.length;
+    const newOffer =
+      (averageValueOfCases * (this.state.turnNum / 10)).toFixed(2);
 
-		this.setState({
-			offer: newOffer,
-		});
-	}
+    this.setState({
+      offer: newOffer,
+    });
+  }
 
-	acceptDeal = () => {
-		alert("You won " + this.formatMoney(this.state.offer) + "!"
-					+ "\nThank you for playing Deal or No Deal!");
-	}
+  acceptDeal = () => {
+    alert("You won " + this.formatMoney(this.state.offer) + "!"
+          + "\nThank you for playing Deal or No Deal!");
+  }
 
-	rejectDeal = () => {
-		const newChoiceNum = ORIGINAL_NUM_CHOICES - this.state.turnNum;
-		const newNumCasesToChoose = newChoiceNum > 1 ?
-			newChoiceNum : 1;
-		this.setState({
-			turnNum: this.state.turnNum + 1,
-			numCasesToChoose: newNumCasesToChoose,
-		});
+  rejectDeal = () => {
+    const newChoiceNum = ORIGINAL_NUM_CHOICES - this.state.turnNum;
+    const newNumCasesToChoose = newChoiceNum > 1 ?
+      newChoiceNum : 1;
+    this.setState({
+      turnNum: this.state.turnNum + 1,
+      numCasesToChoose: newNumCasesToChoose,
+    });
 
-		if (this.state.numCasesRemaining === 1) {
-			this.setState({
-				numCasesToChoose: 0,
-				finalOfferMade: true,
-			})
-		}
-	}
+    if (this.state.numCasesRemaining === 1) {
+      this.setState({
+        numCasesToChoose: 0,
+        finalOfferMade: true,
+      })
+    }
+  }
 
-	keepCase = () => {
-		this.state.offer = this.state.initialCase.value;
+  keepCase = () => {
+    this.state.offer = this.state.initialCase.value;
 
-		this.acceptDeal();
-	}
+    this.acceptDeal();
+  }
 
-	swapCase = () => {
-		const lastCase = [...this.state.cases.filter(c => {
-			return c !== this.state.initialCase && !c.opened;
-		})][0];
+  swapCase = () => {
+    const lastCase = [...this.state.cases.filter(c => {
+      return c !== this.state.initialCase && !c.opened;
+    })][0];
 
-		this.state.offer = lastCase.value;
+    this.state.offer = lastCase.value;
 
-		this.acceptDeal();
-	}
+    this.acceptDeal();
+  }
 
-	formatMoney = (amount, decimalCount = 2, decimal = ".", thousands = ",") => {
-		// Mostly copied from
-		// https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-dollars-currency-string-in-javascript
-	  try {
-	    decimalCount = Math.abs(decimalCount);
-	    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+  formatMoney = (amount, decimalCount = 2, decimal = ".", thousands = ",") => {
+    // Mostly copied from
+    // https://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-dollars-currency-string-in-javascript
+    try {
+      decimalCount = Math.abs(decimalCount);
+      decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
 
-	    let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
-	    let j = (i.length > 3) ? i.length % 3 : 0;
+      let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+      let j = (i.length > 3) ? i.length % 3 : 0;
 
-	    return "$" + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
-	  } catch (e) {
-	    console.log(e);
-	  }
-	}
+      return "$" + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   render() {
     return (
       <div className="App">
-				<Header />
-				<TurnInfo
-					initialCaseNum={this.state.initialCaseNum}
-					numCasesToChoose={this.state.numCasesToChoose}
-					offer={this.state.offer}
-					acceptDeal={this.acceptDeal}
-					rejectDeal={this.rejectDeal}
-					numCasesRemaining={this.state.numCasesRemaining}
-					keepCase={this.keepCase}
-					swapCase={this.swapCase}
-					formatMoney={this.formatMoney}
-					finalOfferMade={this.state.finalOfferMade}/>
+        <Header />
+        <TurnInfo
+          initialCaseNum={this.state.initialCaseNum}
+          numCasesToChoose={this.state.numCasesToChoose}
+          offer={this.state.offer}
+          acceptDeal={this.acceptDeal}
+          rejectDeal={this.rejectDeal}
+          numCasesRemaining={this.state.numCasesRemaining}
+          keepCase={this.keepCase}
+          swapCase={this.swapCase}
+          formatMoney={this.formatMoney}
+          finalOfferMade={this.state.finalOfferMade}/>
         <div className="game-cases-info" style={{
             display: 'flex',
           }}>
           <div className="cases">
             <Cases cases={this.state.cases}
-							chooseCase={this.chooseCase}
-							initialCaseNum={this.state.initialCaseNum} />
+              chooseCase={this.chooseCase}
+              initialCaseNum={this.state.initialCaseNum} />
           </div>
           <li className="spacer" style={{
               visibility: 'hidden',
               flexGrow: '5',
             }} />
-          <ValuesTable
-						cases={this.state.cases}
-						formatMoney={this.formatMoney}/>
+            <ValuesTable
+            cases={this.state.cases}
+            formatMoney={this.formatMoney}/>
         </div>
       </div>
     );
